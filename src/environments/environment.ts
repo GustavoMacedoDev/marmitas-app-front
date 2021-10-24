7651528708e28da6c4737880c11123a932b55471
GustavoMacedoDev <39140951+GustavoMacedoDev@users.noreply.github.com>
2021-10-24 10:30:49 +0000

    24-10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://192.168.2.103:8080/',
+  baseUrl: 'http://localhost:8080/',
  // baseApiUrl: 'http://192.168.0.104:4200/api/'
 };
 
